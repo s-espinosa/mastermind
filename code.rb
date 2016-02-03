159563523d33e2e2e92ade7bf7829e11bff9d1e1
Sal Espinosa <sespinos@gmail.com>
2016-02-02 21:17:28 -0700

    Rewrote Code class and updated tests

--- code.rb
+++ code.rb
@@ -1,58 +1,46 @@
 class Code
-  def initialize        #(num_colors = 4)
-                        #@num_colors = num_colors
-    @code_array = ["R", "G", "G", "B"] #randomize this later, and maybe change to symbols?
+  def initialize
+    @color_choices = ["R", "G", "B", "Y"]
+    @code_array = ["R","G", "G", "B"]
     @code = @code_array.join
   end
 
   attr_reader :code
 
-  def guess(guess_string)
-    guess_array = guess_string.split
-    if guess_array == @code_array
-      time_end = Time.new
-      puts "congrats!"
-    else
-      correct_colors = check_colors(guess_array)
-      correct_position = check_position(guess_array)
-      puts "You got #{correct_colors} colors correct and #{correct_position} in the correct position"
+  def how_many_by_color(color_array)
+    color_hash = {}
+    @color_choices.each do |color|
+      color_hash[color] = 0
     end
+    color_array.each do |color|
+        color_hash[color] += 1
+    end
+    color_hash
   end
 
-  def check_colors(guess_string)
-    guess_array = guess_string.split
-    code_colors_hash = {}
-    num_wrong = 0
 
-    @code_array.each do |letter|
-      if code_colors_hash[letter]
-        code_colors_hash[letter] += 1
-      else
-        code_colors_hash[letter] = 1
-      end
+  def check_colors(guess)
+    guess_array = guess.upcase.split("")
+    code_hash = how_many_by_color(@code_array)
+    guess_hash = how_many_by_color(guess_array)
+
+    total_wrong = 0
+
+    @color_choices.each do |color|
+      total_wrong += code_hash[color] - guess_hash[color]
     end
 
-    guess_array.each do |letter|
-      if code_colors_hash[letter] && code_colors_hash[letter] > 0
-        code_colors_hash[letter] -= 1
-      else
-        num_wrong += 1
-      end
-    end
-
-    num_right = @code_array.length - num_wrong
+    total_right = 4 - total_wrong
   end
 
-  def check_position(guess_string)
-    guess_array = guess_string.split
-    positions_right = 0
+  def check_position(guess)
+    @guess_array = guess.upcase.split("")
+    correct_position = 0
 
-    @code_array.each_with_index do |color, n|
-      if color == guess_array[n]
-        positions_right += 1
-      end
+    @code_array.each_with_index do |color, index|
+      correct_position += 1 if @guess_array[index] == color
     end
 
-    positions_right
+    correct_position
   end
 end
--- code_test.rb
+++ code_test.rb
@@ -4,7 +4,16 @@ require_relative 'code'
 class Code_Test < Minitest::Test
   def test_it_has_a_code
     test_code = Code.new()
-    test_code.code
+    assert !test_code.code.empty?
+  end
+
+  def test_it_returns_hashes_with_color_counts
+    test_code = Code.new()
+
+    guess_array = ["R", "R", "R", "R"]
+    guess_hash = {"R" => 4, "G" => 0, "B" => 0, "Y" => 0}
+    color_hash = test_code.how_many_by_color(guess_array)
+    assert_equal guess_hash, color_hash
   end
 
   def test_it_checks_colors
@@ -12,29 +21,34 @@ class Code_Test < Minitest::Test
 
     guess = "RGRY"
     position_result = test_code.check_colors(guess)
-    position_result <= 4 && position_result >= 0
+    assert_includes 0..4, position_result
   end
 
   def test_it_identifies_all_correct_colors
     test_code = Code.new()
 
     guess = test_code.code
-    test_code.check_colors(guess) == 4
+    assert_equal 4, test_code.check_colors(guess)
+  end
+
+  def test_it_identifies_all_correct_colors_regardless_of_case
+    test_code = Code.new()
+
+    guess = test_code.code.downcase
+    assert_equal 4, test_code.check_colors(guess)
   end
 
   def test_it_checks_position
     test_code = Code.new()
 
     position_result = test_code.check_position("RGYB")
-    position_result <= 4 && position_result >= 0
+    assert_includes 0..4, position_result
   end
 
   def test_it_identifies_all_correct_positions
     test_code = Code.new()
 
     guess = test_code.code
-    test_code.check_position(guess) == 4
+    assert_equal 4, test_code.check_position(guess)
   end
-
-
 end
