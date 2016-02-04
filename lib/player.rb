d368c642f356d45712f2c9eb4373449e93739811
Sal Espinosa <sespinos@gmail.com>
2016-02-04 12:18:29 -0700

    Initial commit Player class

---
+++ lib/player.rb
@@ -0,0 +1,14 @@
+class Player
+  def initialize(name)
+    @name = name.capitalize
+    @current_guess = []
+    @number_of_turns = 0
+  end
+
+  attr_reader :name, :current_guess
+  attr_accessor :number_of_turns
+
+  def guess(guess_string)
+    @current_guess = guess_string.upcase.split("")
+  end
+end
