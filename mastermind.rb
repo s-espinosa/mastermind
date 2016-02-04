bdec279f3cbcb09f0b8ed6f97232a852c5fcf1e3
Sal Espinosa <sespinos@gmail.com>
2016-02-03 20:41:31 -0700

    Add loops to allow multiple guesses

--- mastermind.rb
+++ mastermind.rb
@@ -1,20 +1,26 @@
 require_relative 'play'
-#require_relative 'instructions'
+require_relative 'instructions'
 
 puts "Welcome to MASTERMIND\n"
-choice = ""
 
-puts "Would you like to (p)play, read the (i)nstructions, or (q)uit?"
+def start
+  puts "Would you like to (p)play, read the (i)nstructions, or (q)uit?"
+  choice = ""
+  choice = gets.chomp.split("").first.downcase
 
-choice = gets.chomp.downcase
-
-case choice
-when "p"
-  play()
-when "i"
-  puts "User put i"
-when "q"
-  puts "User put q"
-else
-  puts "Sorry, that's not an option."
+  case choice
+  when "p"
+    player_name = Play.get_player_name
+    new_play = Play.new(player_name)
+    new_play.take_turn
+  when "i"
+    Instructions.print_instructions
+  when "q"
+    #exit
+  else
+    puts "Sorry, that's not an option."
+    start()
+  end
 end
+
+start()
--- player.rb
+++ player.rb
@@ -4,9 +4,11 @@ class Player
   def initialize(name)
     @name = name.capitalize
     @current_guess = []
+    @number_of_turns = 0
   end
 
   attr_reader :name, :current_guess
+  attr_accessor :number_of_turns
 
   def guess(guess_string)
     @current_guess = guess_string.upcase.split("")
