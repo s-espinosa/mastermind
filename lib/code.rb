989fc40cf45866b0d0389803203db35f189662cc
Sal Espinosa <sespinos@gmail.com>
2016-02-05 03:58:06 -0700

    Move helper method

--- lib/code.rb
+++ lib/code.rb
@@ -7,17 +7,6 @@ class Code
 
   attr_reader :code
 
-  def how_many_by_color(color_array)
-    color_hash = {}
-    @color_choices.each do |color|
-      color_hash[color] = 0
-    end
-    color_array.each do |color|
-        color_hash[color] += 1
-    end
-    color_hash
-  end
-
   def check_colors(guess_array)
     code_hash = how_many_by_color(@code_array)
     guess_hash = how_many_by_color(guess_array)
@@ -31,7 +20,6 @@ class Code
     total_right = 4 + total_wrong
   end
 
-
   def check_position(guess_array)
     correct_position = 0
 
@@ -39,5 +27,16 @@ class Code
       correct_position += 1 if guess_array[index] == color
     end
     correct_position
+  end
+
+  def how_many_by_color(color_array)
+    color_hash = {}
+    @color_choices.each do |color|
+      color_hash[color] = 0
+    end
+    color_array.each do |color|
+        color_hash[color] += 1
+    end
+    color_hash
   end
 end
