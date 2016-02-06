dfda7922007f295a3bc4d4b0b463d50836c4dc66
Sal Espinosa <sespinos@gmail.com>
2016-02-06 03:14:05 -0700

    Refactor play

--- lib/play.rb
+++ lib/play.rb
@@ -4,35 +4,38 @@ require_relative 'print_text'
 
 module Play
 
+  # def self.get_name
+  #   puts "Please enter your name:"
+  #   player_name = gets.chomp
+  #   player = Player.new(player_name)
+  #   start_game(player)
+  # end
+
   def self.start_game
-    puts "Please enter your name:"
-    player_name = gets.chomp
-    player = Player.new(player_name)
-    game = Game.new(player)
+    game = Game.new(Player.new)
+    PrintText.short_instructions
+    game.take_turn
+
+    ask_play_again
+  end
+
+  def self.ask_play_again
+    puts "Would you like to play again? (y/n)"
+    play_again = CleanText.getsmall
+
+    check_play_again(play_again)
+  end
 
 
-    play_again = true
-    until play_again == false
-      PrintText.short_instructions
-      game.take_turn
-
-      acceptable = false
-      until acceptable == true
-        puts "Would you like to play again? (y/n)"
-        play_again = CleanText.getsmall
-
-        case play_again
-        when "y"
-          play_again = true
-          acceptable = true
-        when "n"
-          play_again = false
-          acceptable = true
-        else
-          puts "Sorry, that's not an option.\n\n"
-          acceptable = false
-        end
-      end
+  def self.check_play_again(play_again)
+    if play_again == "y"
+      start_game
+    elsif play_again == "n"
+      abort
+    else
+      puts "Sorry, that's not an option.\n\n"
+      ask_play_again
     end
   end
+
 end
