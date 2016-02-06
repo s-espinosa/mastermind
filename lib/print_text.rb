695891321760b951d07f0f537116ae068cf05688
Sal Espinosa <sespinos@gmail.com>
2016-02-05 21:28:05 -0700

    Remove width check

--- lib/print_text.rb
+++ lib/print_text.rb
@@ -1,30 +1,25 @@
 module PrintText
   def self.intro
-    line_width = `/usr/bin/env tput cols`.to_i
-    if line_width != nil && line_width >= 80
-      puts "WELCOME TO
+    puts "WELCOME TO
 ___  ___  ___   _____ _____ ______________  ________ _   _______
 |  \\/  | / _ \\ /  ___|_   _|  ___| ___ \\  \\/  |_   _| \\ | |  _  \\
 | .  . |/ /_\\ \\\\ `--.  | | | |__ | |_/ / .  . | | | |  \\| | | | |
 | |\\/| ||  _  | `--. \\ | | |  __||    /| |\\/| | | | | . ` | | | |
 | |  | || | | |/\\__/ / | | | |___| |\\ \\| |  | |_| |_| |\\  | |/ /
 \\_|  |_/\\_| |_/\\____/  \\_/ \\____/\\_| \\_\\_|  |_/\\___/\\_| \\_/___/  "
-    else
-      puts "Welcome to MASTERMIND\n"
-    end
   end
 
   def self.long_instructions
-    puts "\n__________________________________________________________________________________________"
+    puts "\n_______________________________________________________________________________________"
     puts "\nWelcome to MASTERMIND. This is a game of guessing. I am going to pick a four color code \nusing red, green, blue, and yellow. Your job is to guess the code!\n\n"
     puts "After each guess, I will tell you the number of colors that you guessed correctly, \nand the number of colors that you guessed in the correct space.\n\n"
     puts "Guess all four colors correctly, and you win!\n\n"
-    puts "__________________________________________________________________________________________\n\n"
+    puts "_______________________________________________________________________________________\n\n"
   end
 
   def self.short_instructions
-    puts "\n__________________________________________________________________________________________"
+    puts "\n_______________________________________________________________________________________"
     puts "\nI have generated a beginner sequence with four elements made up of: (r)ed,(g)reen, \n(b)lue, and (y)ellow.\n\nUse (q)uit at any time to end the game."
-    puts "\n__________________________________________________________________________________________\n\n"
+    puts "\n_______________________________________________________________________________________\n\n"
   end
 end
