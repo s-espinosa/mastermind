52e17b9bcc347d6e5857d910b9536b15e9d7ce27
Sal Espinosa <sespinos@gmail.com>
2016-02-06 09:52:33 -0700

    Remove test for deleted method/update Player.new calls

--- test/code_test.rb
+++ test/code_test.rb
@@ -9,19 +9,9 @@ class Code_Test < Minitest::Test
     assert !test_code.code.empty?
   end
 
-  def test_it_returns_hashes_with_color_counts
-    test_code = Code.new()
-    test_player = Player.new("George")
-
-    guess_array = test_player.guess("rrrr")
-    guess_hash = {"R" => 4, "G" => 0, "B" => 0, "Y" => 0}
-    color_hash = test_code.how_many_by_color(guess_array)
-    assert_equal guess_hash, color_hash
-  end
-
   def test_it_checks_colors
     test_code = Code.new()
-    test_player = Player.new("George")
+    test_player = Player.new
 
     guess = test_player.guess("RGRY")
     position_result = test_code.check_colors(guess)
@@ -30,7 +20,7 @@ class Code_Test < Minitest::Test
 
   def test_it_identifies_all_correct_colors
     test_code = Code.new()
-    test_player = Player.new("George")
+    test_player = Player.new
 
     guess = test_player.guess(test_code.code)
     assert_equal 4, test_code.check_colors(guess)
@@ -40,7 +30,7 @@ class Code_Test < Minitest::Test
 
   def test_it_checks_position
     test_code = Code.new()
-    test_player = Player.new("George")
+    test_player = Player.new
 
     position_result = test_code.check_position(test_player.guess("RGYB"))
     assert_includes 0..4, position_result
@@ -48,7 +38,7 @@ class Code_Test < Minitest::Test
 
   def test_it_identifies_all_correct_positions
     test_code = Code.new()
-    test_player = Player.new("George")
+    test_player = Player.new
 
     guess = test_player.guess(test_code.code)
     assert_equal 4, test_code.check_position(guess)
