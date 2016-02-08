ab40ee537320313c98bc7901cda61210c6c9541a
Sal Espinosa <sespinos@gmail.com>
2016-02-08 12:47:53 -0700

    Delete historical files and let Git do what it's supposed to do

--- iteration1/instructions.rb
+++
@@ -1,8 +0,0 @@
-module Instructions
-  def self.print_instructions
-    puts "\nWelcome to MASTERMIND. This is a game of guessing. I am going to pick a four color code \nusing red, green, blue, and yellow. Your job is to guess the code!\n\n"
-    puts "After each guess, I will tell you the number of colors that you guessed correctly, \nand the number of colors that you guessed in the correct space.\n\n"
-    puts "Guess all four colors correctly, and you win!\n\n"
-    start()
-  end
-end
--- iteration1/play_test.rb
+++
@@ -1,8 +0,0 @@
-require 'minitest/autorun'
-require_relative 'play'
-
-class Play_Test < Minitest::Test
-  def test_it_has_a_start_time
-  end
-
-end
